f9a57247404814248529a366e19aa2facb697ec9
Erick Castille <ecastille924@gmail.com>
2020-12-28 18:13:59 -0500

    created a seeds.rb file and populated two teams worth of data

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Team.create(name: "Brooklyn Nets", coach: "Steve Nash", point_guard: "Spencer Dinwiddie", shooting_guard: "Kyrie Irving", small_forward: "Joe Harris", power_forward: "Kevin Durant", center: "DeAndre Jordan")
+Team.create(name: "New York Knciks", coach: "Tom Thibodeau", point_guard: "Elfrid Payton", shooting_guard: "Reggie Bullock", small_forward: "RJ Barrett", power_forward: "Julius Randle", center: "Mitchell Robinson")
