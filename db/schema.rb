4953f26e4a3995483ab46b92d78aed8cd2ec7ce8
Erick Castille <ecastille924@gmail.com>
2021-01-01 17:43:24 -0500

    added a foreign key to the teams table to set up the has man/belongs to relationship between user and teams

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210101181532) do
+ActiveRecord::Schema.define(version: 20210101223922) do
 
   create_table "teams", force: :cascade do |t|
-    t.string "name"
-    t.string "coach"
-    t.string "point_guard"
-    t.string "shooting_guard"
-    t.string "small_forward"
-    t.string "power_forward"
-    t.string "center"
+    t.string  "name"
+    t.string  "coach"
+    t.string  "point_guard"
+    t.string  "shooting_guard"
+    t.string  "small_forward"
+    t.string  "power_forward"
+    t.string  "center"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
