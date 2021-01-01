c0c1848449cec9c7f5a7e143316e6f4b9b9abd45
Erick Castille <ecastille924@gmail.com>
2021-01-01 17:07:48 -0500

    added code in the footer to confirm to the user that they are logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,7 @@ class ApplicationController < Sinatra::Base
       if @current_user 
         @current_user 
       else
-        @current_user = User.find_by(id: session[user_id])
+        @current_user = User.find_by(id: session[:user_id])
       end
     end
   end
