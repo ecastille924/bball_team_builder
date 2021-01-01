3f694b1ae86048e04e1cd2876a2d5421e63d057e
Erick Castille <ecastille924@gmail.com>
2021-01-01 17:23:40 -0500

    added post request fo users_controller for loggin in a user.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,15 @@ class UsersController < ApplicationController
         session.clear 
         redirect '/login'
     end
+
+    post "/login" do 
+        user = User.find_by(username: params[:user][:username])
+        if
+            user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id 
+            redirect '/teams'
+        else 
+            redirect '/login'
+        end
+    end
 end
