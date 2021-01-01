2c5f9000f61826735873588b02106e8ed89ee836
Erick Castille <ecastille924@gmail.com>
2021-01-01 13:40:41 -0500

    wrote out more of the new users form and wrote a post request in the UsersController to load submit the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
     get "/signup" do 
         erb :"users/new"
     end
+
+    post "/signup" do 
+        user = User.create(params[:user])
+    end
 end
