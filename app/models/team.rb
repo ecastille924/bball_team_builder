83b77a541f6494f7414598577a81e68d224954e3
Erick Castille <ecastille924@gmail.com>
2021-01-02 15:03:49 -0500

    added validation for all fields in the new team form.

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,10 @@
 class Team <ActiveRecord::Base 
     belongs_to :user
     validates :name, presence: true 
+    validates :point_guard, presence: true 
+    validates :shooting_guard, presence: true 
+    validates :power_forward, presence: true 
+    validates :small_forward, presence: true 
+    validates :center, presence: true 
+    validates :coach, presence: true 
 end
